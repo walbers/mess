6deffaa952239c1d93c349b924deccb81a213bdd
will <walbers2@illinois.edu>
2024-04-16 02:15:07 -0500

    starting to add rust

--- README.md
+++ README.md
@@ -7,12 +7,18 @@ Building projects can take a long time and I wanted to get a notification when t
 
 - Build
     - `cargo build`
+    - `sudo cp target/debug/mess /usr/local/bin/mess`
+- Config
+    - edit `mess.ini` and copy it to `~/.mess.ini`
 - Run
-    - `MESS_DURATION=10 MESS_MESSENGER=desktop mess/target/debug/mess <your bash command with args>`
+    - `mess <bash command>`
 - For WSL you will need to setup `wsl-notify-send` for desktop notifications. You can follow this [blog](https://stuartleeks.com/posts/wsl-github-cli-windows-notifications-part-1/) or my steps below
-    - `wget https://github.com/stuartleeks/wsl-notify-send/releases/download/v0.1.871612270/wsl-notify-send_windows_amd64.zip`
-    - `unzip wsl-notify-send_windows_amd64.zip`
-    - Add the path of `wsl-notify-send.exe` to `src/main.rs:73` and rebuild
+
+    ```bash
+    wget https://github.com/stuartleeks/wsl-notify-send/releases/download/v0.1.871612270/wsl-notify-send_windows_amd64.zip
+    unzip wsl-notify-send_windows_amd64.zip
+    sudo cp wsl-notify-send.exe /usr/local/bin/notify-send
+    ```
 
 ## Integrations
 
@@ -20,11 +26,11 @@ Building projects can take a long time and I wanted to get a notification when t
 
 - Desktop notifications
     - Will work in WSL with [wsl-notify-send](https://github.com/stuartleeks/wsl-notify-send)
+- Discord
 
 ### Future
 
-- Discord
-- Push notification
 - Slack
+- Push notification
 - Text
     - requires twilio API key
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,55 @@ use std::time::Instant;
 use std::io::{BufReader, BufRead};
 use std::collections::HashMap;
 use std::env;
+use ini::Ini;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready, id::UserId},
+    prelude::*,
+};
 
 const AVAILABLE_MESSENGERS: [&str; 1] = ["desktop"];
 
-fn get_mess_env_variables() -> HashMap<String, String> {
+fn get_mess_env_settings() -> HashMap<String, String> {
     let mut mess_variables: HashMap<String, String> = HashMap::new();
     for (key, value) in env::vars() {
         if key.starts_with("MESS_") {
-            mess_variables.insert(key, value);
+            mess_variables.insert(key[5..].to_string(), value);
         }
     }
     mess_variables
 }
 
+
+fn get_settings() -> HashMap<String, String> {
+    let home_dir = env::var("HOME").unwrap();
+    let config_path = format!("{}/.mess.ini", home_dir);
+    let conf = Ini::load_from_file(config_path).unwrap();
+
+    let mut mess_settings: HashMap<String, String> = HashMap::new();
+    for (sec, prop) in &conf {
+        println!("Section: {:?}", sec);
+        for (key, value) in prop.iter() {
+            println!("{:?}:{:?}", key, value);
+            mess_settings.insert(key.to_string(), value.to_string());
+        }
+    }
+
+    let env_settings = get_mess_env_settings();
+    for (key, value) in env_settings {
+        mess_settings.insert(key, value);
+    }
+
+    mess_settings
+}
+
 fn get_messengers(mess_variables: &HashMap<String, String>) -> Vec<String> {
-    if !mess_variables.contains_key("MESS_MESSENGERS") {
+    if !mess_variables.contains_key("MESSENGERS") {
         eprintln!("No messengers found!");
         exit(1);
     }
 
-    let messengers_str =  mess_variables.get("MESS_MESSENGERS").unwrap();
+    let messengers_str =  mess_variables.get("MESSENGERS").unwrap();
     let messengers: Vec<String> = messengers_str.split(',').map(|s| s.to_string()).collect();
     for messenger in &messengers {
         if !AVAILABLE_MESSENGERS.contains(&messenger.as_str()) {
@@ -34,22 +63,20 @@ fn get_messengers(mess_variables: &HashMap<String, String>) -> Vec<String> {
 }
 
 fn get_duration_allowed(mess_variables: &HashMap<String, String>) -> u64 {
-    if !mess_variables.contains_key("MESS_DURATION") {
+    if !mess_variables.contains_key("DURATION") {
         eprintln!("No duration allowed found!");
         exit(1);
     }
 
-    let duration_allowed_str = mess_variables.get("MESS_DURATION").unwrap();
+    let duration_allowed_str = mess_variables.get("DURATION").unwrap();
     let duration_allowed = duration_allowed_str.parse::<u64>().unwrap_or_else(|e| {
-        eprint!("Failed to parse MESS_DURATION to integer: {}", e);
+        eprint!("Failed to parse DURATION to integer: {}", e);
         exit(1);
     });
     duration_allowed
 }
 
 fn run_the_program(program_to_run: &[String]) {
-    // println!("{}", program_to_run[0].clone());
-    // println!("{}", program_to_run);
     let mut child = Command::new(&program_to_run[0])
     .args(&program_to_run[1..])
     .stdout(Stdio::piped())
@@ -68,7 +95,7 @@ fn run_the_program(program_to_run: &[String]) {
     let _status = child.wait().expect("Command wasn't running");
 }
 
-fn send_notify_send_message(program_name: &String, duration: u64) {
+fn send_desktop_message(program_name: &String, duration: u64) {
     let message = format!("Your program {} has finished after {} minutes", program_name, duration);
     let mut child = Command::new("notify-send")
     .arg("--category")
@@ -80,30 +107,33 @@ fn send_notify_send_message(program_name: &String, duration: u64) {
     let _status = child.wait().expect("Command wasn't running");
 }
 
-fn send_message(messengers: Vec<String>, program_name: &String, duration: u64) {
+async fn send_discord_message(program_name: &String, duration: u64, mess_settings: HashMap<String, String>) {
+
+}
+
+async fn send_message(program_name: &String, duration: u64, mess_settings: HashMap<String, String>) {
+    let messengers = get_messengers(&mess_settings);
     for messenger in messengers {
         match &messenger as &str {
-            "desktop" => send_notify_send_message(&program_name, duration),
+            "desktop" => send_desktop_message(&program_name, duration),
+            "discord" => send_discord_message(&program_name, duration, mess_settings),
             _ => eprint!("The messenger {} doesn't exist. Skipping...", messenger),
         }
     }
 }
+#[tokio::main]
+async fn main() {
+    // HANDLE case just `mess` or `mess --help --version and --test to make sure message works`
 
-fn main() {
     // collect arguements and settings
-    let mess_env_variables = get_mess_env_variables();
-    let messengers = get_messengers(&mess_env_variables);
-    let duration_allowed = get_duration_allowed(&mess_env_variables);
-
-
+    let mess_settings = get_settings();
+    let messengers = get_messengers(&mess_settings);
+    let duration_allowed = get_duration_allowed(&mess_settings);
     let args_passed: Vec<String> = env::args().collect();
-    let program_to_run = &args_passed[1..];
-    // program_to_run.remove(0); // Starts with the name of this program
-    let program_name = &program_to_run[0];
 
     // run the program
     let start = Instant::now();
-    run_the_program(program_to_run);
+    run_the_program(&args_passed[1..]);
     let duration = start.elapsed();
 
     // send message
@@ -111,7 +141,7 @@ fn main() {
     println!("Duration: {:?}", duration);
     println!("Duration allowed: {:?}", duration_allowed);
     if duration.as_secs() >= duration_allowed { // TODO: *60
-        send_message(messengers, program_name, duration.as_secs());
+        send_message(&args_passed[1], duration.as_secs(), mess_settings);
     } else {
         println!("Execution was less than allowed duration. No message sent.");
     }
